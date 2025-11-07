a162cc6bea60b26985482c6e5220ed43bcaf8f59
11yubin <11yubine@gmail.com>
2025-11-07 19:29:49 +0900

    fix: config file

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,14 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # .env 파일에서 읽어올 변수들
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
     AWS_DEFAULT_REGION: str = "us-east-1"
     BEDROCK_MODEL_ID: str = "anthropic.claude-3-sonnet-20240229-v1:0"
+
+    REDDIT_CLIENT_ID: str
+    REDDIT_CLIENT_SECRET: str
+    REDDIT_PASSWORD: str
+    REDDIT_USER_AGENT: str
+    REDDIT_USERNAME: str
 
     # DB 파일 경로
     DB_PATH: str = "kfood_recipes.db"
